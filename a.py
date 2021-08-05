c18daab1a97fbb417decf37b7bf06243c17ba503
SaintLaurentxTran <thanhmoicotoi@gmail.com>
2021-08-06 00:02:32 +0700

    daoli

--- a.py
+++ a.py
@@ -1,3 +1,10 @@
-# có làm thì mới có ăn
-# thanh mòi đẹp trai vcl
-# muốn thành công thì phải trải qua đắng cay ngọt bùi
+# em chỉ nói với anh câu này thôi em có sai với ai đi nữa, em có làm cái gì đi nữa nếu có phải trả giá, em cũng xin chấp nhận.
+# Bởi vì anh, anh biết đấy ra xã hội làm ăn bươn chải liều thì ăn nhiều, không liều thì ăn ít.
+# Muốn thành công thì phải chấp nhận trải qua đắng cay ngọt bùi, làm ăn muốn kiếm được tiền phải chấp nhận mạo hiểm, nguy hiểm một tý nhưng trong tầm kiểm soát, anh hiểu chưa.
+# Xã hội này, chỉ có làm chịu khó cần cù thì bù siêng năng, chỉ có làm thì mới có ăn, những cái loại không làm mà đòi có ăn thì ăn ĐẦU BUỒI, ăn CỨT. Thế cho nó dễ
+# Xã hội này không làm chỉ có ăn cứt thôi mình nói thế cho nó nhanh cho nó dễ hiều. Còn xã hội này sống nếu muốn người ta tôn trọng, mình phải tôn trọng người khác trước, muốn người ta quý mình, mình phải quý người ta trước.
+# Những người cho mình vay tiền, lúc mình khó khăn, lúc mình khổ, lúc mình vỡ nợ. Không phải người ta ngu, không phải người ta dại.
+# Mà chính vì những người đó mình phải nên tôn trọng, bởi vì những người đó coi mình là anh em bạn bè thì lúc mình vợ nợ, khó khăn, khổ người ta mới đưa tiền cho mình vay.
+# Những người có tiền không phải người ta ngu đâu, không phải người ta thừa tiền đâu. Người ta coi mình quan trọng hơn tiền bạc, người ta mới giúp mình lúc khó khăn lúc khổ.
+# Còn, anh em bạn bè chơi với nhau có ăn thì tìm dến, có lồn! À có đòn thì tìm đi, những cái loại đấy là anh em mà anh anh. Ối dồi ôi, nhá!
+# Đúng rồi anh em ạ, cuộc sống thì không giống cuộc đời.
